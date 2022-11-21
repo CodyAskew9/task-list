cf8c2907df30e0ffb1d4c0ca56d76d524c9cf606
cody askew <codyaskew9@gmail.com>
2022-11-20 20:13:12 -0700

    work work

--- README.old.md
+++
@@ -1,4 +0,0 @@
-# task-list
-a day planner to increase my efficiency
-this is a simple project that most people do using React but I dont like doing what most people do so I am late to doing it.
-but I plan to add some fun extras so enjoy!
--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCircleCheck, faPen, faTrashCan
+ } from "@fortawesome/free-solid-svg-icons"
 import './App.css';
 
 function App() {
+ 
+const [toDo, setToDO] = useState([
+  {"id":1, "title": "task 1", "status": false},
+  {"id":1, "title": "task 2", "status": false}
+])
+
+const [newTask, setNewTask] = useState('')
+const [update, setUpdate] = useState('')
+const [score , setScore] =useState(0)
+
+// delete tasks
+const deleteTask = (id) => {}
+
+// update tasks
+const updateTasks = () => {}
+
+// cancel update
+const cancelUpdateTasks = () => {}
+
+// complete tasks and update score
+const completeTasks = (id) =>{}
+
+// Change task for update
+const changeTasks = (e) =>{}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container App">
+      <br />
+      <br />
+      <h1>Tasks</h1>
+      <br />
+      <br />
+      
+      {toDo && toDo.length ? '' : 'Jobs Done!'}
+
+      {toDo && toDo.map((task, index) => {
+        return(
+          <React.Fragment key={task.id}>
+            <div className='col taskBg'>
+              <div className={task.status ? 'done' : ''}>
+
+          <span className='taskNumber'>{index +1}</span>
+          <span className='taskText'>{task.title}</span>
+              </div>
+              <div className='iconWrap'>
+                <span><FontAwesomeIcon icon={faCircleCheck}/></span>
+                <span><FontAwesomeIcon icon={faPen}/></span>
+                <span><FontAwesomeIcon icon={faTrashCan}/></span>
+              </div>
+            </div>
+
+          </React.Fragment>
+        )
+      })
+      }
+
+      <div className='container'>
+        
+      </div>
+
     </div>
   );
 }
